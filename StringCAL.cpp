f2fd5eb93d6ef4fbf4bd602db3d8de76a13e171a
LHostAd <adele.kersaint@hotmail.fr>
2016-02-15 17:14:37 +0100

    +Constructeur par copie

--- StringCAL.cpp
+++ StringCAL.cpp
@@ -14,6 +14,15 @@ StringCAL::StringCAL() {
   capacity_ = 0;
 }
 
+StringCAL :: StringCAL(const StringCAL & copied){
+  size_ = copied.size_;
+  capacity_ = copied.capacity_;
+  ptr_ = new char[size_];
+  for (unsigned int i=0;i<size_;i++){
+    ptr_[i] = copied.ptr_[i];
+  }
+}
+
 //=========================== Destructor ===============================
 StringCAL::~StringCAL() {
   delete[] ptr_;
